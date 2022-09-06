858ca2d17fb36baec996a4d694dc7421398462b0
simonwandera <simonwandera12@gmail.com>
2022-09-06 10:52:36 +0300

    enabled a gun to shoot

--- src/com/wargame/Gun.java
+++ src/com/wargame/Gun.java
@@ -2,48 +2,9 @@ package com.wargame;
 
 public class Gun {
     int bullets;
-    int maxBullets;
-    String gunSize;
-
-    public String getGunSize() {
-        return gunSize;
-    }
-
-    public void setGunSize(String gunSize) {
-        this.gunSize = gunSize;
-    }
-
-    public void reloadBullets() {
-        bullets = maxBullets;
-    }
-
 
     public Gun(String size, int bullets) {
-        this.maxBullets = 5;
-        reloadBullets();
-        this.gunSize = size;
         this.bullets = bullets;
-    }
-
-    public void shootMissiles() {
-        if (bullets > 0)
-        {
-            if (this.gunSize.equals("small")){
-                bullets --;
-                System.out.println("------");
-            }
-            else if(this.gunSize.equals("medium")) {
-                bullets --;
-                System.out.println("---------------");
-            } else if (this.gunSize.equals("large")) {
-                bullets --;
-                System.out.println("------------------------------");
-                
-            }
-        }else {
-            System.out.println("Reloadin ...");
-            this.setBullets(15);
-        }
     }
     public int getBullets() {
         return bullets;
@@ -52,12 +13,8 @@ public class Gun {
     public void setBullets(int bullets) {
         this.bullets = bullets;
     }
-
-    public int getMaxBullets() {
-        return maxBullets;
-    }
-
-    public void setMaxBullets(int maxBullets) {
-        this.maxBullets = maxBullets;
+    
+    public void shoot(){
+        this.bullets--;
     }
 }
